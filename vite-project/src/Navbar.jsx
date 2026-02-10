8f5808d4876b50e8bee26d5d07d2b621b64707c0
Kaitlyn Brewer <kaitlynmbrewer@gmail.com>
2026-02-09 20:35:57 -0500

    added links to navbar, updated CSS, updated webpage title

--- vite-project/src/Navbar.jsx
+++ vite-project/src/Navbar.jsx
@@ -4,7 +4,7 @@ function Navbar() {
   return (
     <div className="navbar-container">
       <h1>Random Cat Facts</h1>
-      <div className="links"></div>
+      <div className="links"><a href="https://catfact.ninja/">API docs</a> | <a href="https://github.com/kaitbrew">Github</a></div>
     </div>
   );
 }
