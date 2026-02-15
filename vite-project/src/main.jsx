364892c7657f93f020d0a7da8b1c9eb4364f24ba
Kaitlyn Brewer <kaitlynmbrewer@gmail.com>
2026-02-15 11:38:35 -0500

    footer complete

--- vite-project/src/main.jsx
+++ vite-project/src/main.jsx
@@ -3,10 +3,12 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import Navbar from './Navbar.jsx'
+import Footer from './Footer.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Navbar />
     <App />
+    <Footer />
   </StrictMode>,
 )
